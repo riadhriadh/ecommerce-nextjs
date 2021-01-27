79ce5e0034edc48bfd086182cfd857fe7a2c96e2
riadh <riadh.farhati@allocab.com>
2021-01-27 13:57:39 +0100

    first

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Page from '../components/page';
 import ProductSection from '../components/productSection';
 
 export default function Index() {
+  
   return (
     <Page>
       <ProductSection />
